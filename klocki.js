920839ebb7fe87a971e11312ecfceb4de9ffb4ff
Tomasz Duda <tomasz.duda.krk@gmail.com>
2019-07-22 20:12:19 +0200

    added useless function

---
+++ klocki.js
@@ -0,0 +1,11 @@
+const appendPhoto = (url) => {
+    const img = new Image();
+    img.src = url;
+    $('body').append(img);
+}
+setInterval(() => {
+    $.get('https://jsonplaceholder.typicode.com/photos', function(data){
+        const rand = Math.floor(Math.random() * 5000);
+        appendPhoto(data[rand].url)
+    })
+}, 3000);
